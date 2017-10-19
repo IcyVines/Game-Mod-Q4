95c1dd9f937268d32ab46e0e10ba31aba5a341e0
Roy <IcyVines@users.noreply.github.com>
2017-10-19 13:01:43 -0400

    Actually added files through git add

---
+++ game/mod/ModRMK.cpp
@@ -0,0 +1,4 @@
+#include "ModRMK.h"
+
+char *RMKstatNames[] = { "dexterity","perception","agility","vitality","endurance","luck" };
+char *RMKclassNames[] = { "cowboy","ranger","deadeye","vanguard","juggernaut" };
---
+++ game/mod/ModRMK.h
@@ -0,0 +1,21 @@
+
+#pragma once
+//Mod
+enum StatTypes {
+	Dexterity,
+	Perception,
+	Agility,
+	Vitality,
+	Endurance,
+	Luck
+};
+extern char *RMKstatNames[];
+enum ClassTypes {
+	Cowboy,
+	Ranger,
+	Deadeye,
+	Vanguard,
+	Juggernaut
+};
+extern char *RMKclassNames[];
+//Mod End
